ab6197b478202399e2da69f19580b1a06ed14b46
imontalvomiguel <ubaldo.im@gmail.com>
2018-09-30 17:50:06 -0500

    Helpers tags scripts & styles

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 class App < Sinatra::Base
   use DevServerProxy if development?
 
+  helpers do
+    def javascript_pack_tag(name)
+      "<script src=\"/packs/#{name}.js\"></script>"
+    end
+
+    def stylesheet_pack_tag(name)
+      if settings.environment == :production
+        "<link rel=\"stylesheet\" href=\"/packs/#{name}.css\" />"
+      end
+    end
+  end
+
   get '/' do
     erb :app
   end
